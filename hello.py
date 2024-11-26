7eddbbef1a341ecd6af17c1ec3728af5ed7b00b8
Iago Rodrigues <iagorodriguesn.io@gmail.com>
2024-11-26 14:02:41 -0300

    padronizando

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python3
+"""
+ Multiplas Línguas
+
+"""
+__version__ = "0.0.1"
+__author__  = "Iago Rodrigues"
+__license__ = "Unlicense"
 
 def main():
-    print("Hello from python-base!")
+    print(__license__)
 
 
 if __name__ == "__main__":
     main()
+
+
