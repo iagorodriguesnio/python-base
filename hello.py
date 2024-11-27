cb8d6cb60dcab5a5d7644858e012f9fbf4acb219
Iago Rodrigues <iagorodriguesn.io@gmail.com>
2024-11-26 22:40:55 -0300

    implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -7,11 +7,26 @@ __version__ = "0.0.1"
 __author__  = "Iago Rodrigues"
 __license__ = "Unlicense"
 
-def main():
-    print(__license__)
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello World!"
 
 
-if __name__ == "__main__":
-    main()
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg) 
+
+#if __name__ == "__main__":
+ #   main()
 
 
