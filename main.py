a1a9073b55e636ca0f9f81cf1774ea5ea8f7580e
Iago Rodrigues <iagorodriguesn.io@gmail.com>
2025-05-28 17:50:06 -0300

    olá mundo em varias linguas

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hello')
+
+__version__ = "0.0.1"
+__author__ = "Iago Rodrigues"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG","en_US")
+
+
+msg = "Hello World!"
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo!"
+
+if current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+if current_language == "es_SP":
+    msg = "Hola, Mundo!"
+
+if current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
