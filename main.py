d36755453efaf6a9cdb07a7d0903816ba5fdec75
Iago Rodrigues <iagorodriguesn.io@gmail.com>
2025-10-06 22:21:29 -0300

    seg commit

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from python-base!")
 
+"""
+Hello World Multi Linguas
 
-if __name__ == "__main__":
-    main()
+Dependendo da linguagem configurada no ambiente o programa exibe a correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex: 
+   
+    export LANG=pt_BR
+
+Execução: 
+
+    python main.py
+
+"""
+__version__ = "0.0.1"
+__author__ = "Iago Rodrigues"
+__license__ = "Unlicense"
+
+current_language = "en_US"
+
+msg = "Hello World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
